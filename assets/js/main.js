354e67fdf4cd23407195cc52412262bd2be37700
TurksoyAz <supercoder@AirvonSperCoder.fritz.box>
2022-08-20 16:21:27 +0200

    Projekt

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+
+
+function bot1() {
+    brek.style.color="#97f5bb";
+    let a = document.getElementById("brek").innerHTML;
+    let b = Number(a) + Number(1);
+    document.getElementById("brek").innerHTML = b;}
+
+function bot2() { 
+    brek.style.color="#97f5bb";
+    let artik = document.getElementById("brek").innerHTML; 
+    let artik2 = Number(artik) + Number(2);
+    document.getElementById("brek").innerHTML=artik2;}
+
+function bot3(){
+    brek.style.color="#97f5bb";
+    let c = document.getElementById("brek").innerHTML;
+    let d = Number(c) + Number(3);
+    document.getElementById("brek").innerHTML = d;}
+
+
+/* ===================  */
+
+function botto1(){
+    fixo.style.color="#525DF4"
+    let e = document.getElementById("fixo").innerHTML;
+    let f = Number(e) + Number(1);
+    document.getElementById("fixo").innerHTML= f;
+}
+
+function botto2(){
+    fixo.style.color="#525DF4"
+    let g = document.getElementById("fixo").innerHTML;
+    let h = Number(g) + Number(2);
+    document.getElementById("fixo").innerHTML=h;
+}
+
+function botto3(){
+    fixo.style.color="#525DF4"
+    let m = document.getElementById("fixo").innerHTML;
+    let n = Number(m) + Number(3);
+    document.getElementById("fixo").innerHTML=n;
+}
+
+ function spon() {
+     document.getElementById("brek").innerHTML= "0";
+     document.getElementById("fixo").innerHTML= "0";
+  }
+
+
+  
+function prompt1(){
+
+let a =prompt("Fügen Sie den Name des ersten Teams ein?");
+document.getElementById("hersog").innerHTML =a 
+
+let b =prompt("Fügen Sie den Name des zweiten Teams ein?");
+document.getElementById("triko").innerHTML =a 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
